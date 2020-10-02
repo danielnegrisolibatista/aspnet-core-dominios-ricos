e16f7e6454b22505d6a7c370c3139ea525757a33
Daniel Negrisoli Batista <danielnegrisoli@gmail.com>
2020-10-02 15:31:17 -0300

    Refactoring Domain Entities

---
+++ PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        public void AdicionarSubscriptionTest()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Daniel", "Negrisoli Batista", "123459789", "daniel@daniel.com.br");
+
+            student.AddSubscription(subscription);
+        }
+    }
+}
