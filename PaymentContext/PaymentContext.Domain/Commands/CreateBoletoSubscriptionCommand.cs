7ff6b80a81861122dd87627f1ed7fe951054b68d
Daniel Negrisoli Batista <danielnegrisoli@gmail.com>
2020-10-04 12:17:34 -0300

    Added Domain Commands

---
+++ PaymentContext/PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
@@ -0,0 +1,50 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+using PaymentContext.Shared.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentContext.Domain.Commands
+{
+    public class CreateBoletoSubscriptionCommand : Notifiable, ICommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+
+        public string BarCode { get; set; }
+        public string BoletoNumber { get; set; }
+
+        public string PaymentNumber { get; set; }
+        public DateTime PaidDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public decimal Total { get; set; }
+        public decimal TotalPaid { get; set; }
+        public string Payer { get; set; }
+        public string PayerEmail { get; set; }
+        public Document PayerDocument { get; set; }
+        public EDocumentType PayerDocumentType { get; set; }
+
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Neighborhood { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Contry { get; set; }
+        public string ZipCode { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(FirstName, 3, "Name.FirstName", "Nome de conter pelo menos 3 caracteres")
+                .HasMinLen(LastName, 3, "Name.LastName", "Nome de conter pelo menos 3 caracteres")
+                .HasMaxLen(FirstName, 40, "Name.FirstName", "Nome de conter no máximo 40 caracteres")
+            );
+        }
+    }
+}
---
+++ PaymentContext/PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using PaymentContext.Domain.Commands;
+
+namespace PaymentContext.Tests.Commands
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        [TestMethod]
+        public void ShouldReturnErrorWhenNameIsInvalid()
+        {
+            var command = new CreateBoletoSubscriptionCommand();
+            command.FirstName = "";
+
+            command.Validate();
+            Assert.AreEqual(false, command.Valid);
+        }
+    }
+}
